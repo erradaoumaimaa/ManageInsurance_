6dafae598ac73dd443bd90799a680880f106daf7
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-10-23 15:09:25 +0100

    MA-7 Create User Model

--- src/main/java/com/manageInsurance/entities/User.java
+++ src/main/java/com/manageInsurance/entities/User.java
@@ -1,6 +1,8 @@
 package com.manageInsurance.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "users")
@@ -8,15 +10,32 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    protected Long id;
 
+    @NotEmpty(message = "Name is required")
     @Column(name = "name", nullable = false)
-    private String name;
+    protected String name;
+
+    @NotEmpty(message = "Address is required")
+    @Column(name = "address", nullable = false)
+    protected String address;
+
+    @Email(message = "Email should be valid")
+    @NotEmpty(message = "Email is required")
+    @Column(name = "email", unique = true, nullable = false, length = 100)
+    protected String email;
+
+    @NotEmpty(message = "Password is required")
+    @Column(name = "password", nullable = false, length = 255)
+    protected String password;
 
     public User() {}
 
-    public User(String name) {
+    public User(String name, String address, String email, String password) {
         this.name = name;
+        this.address = address;
+        this.email = email;
+        this.password = password;
     }
 
     // Getters and Setters
@@ -34,5 +53,29 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
